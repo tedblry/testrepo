093486ece72d604593475ff867ffbc631bd479e2
tedblry <104002228+tedblry@users.noreply.github.com>
2022-04-19 20:14:30 +0900

    Add files via upload
    adding any random file to the repository

---
+++ apostrophe.py
@@ -0,0 +1,28 @@
+message = "One of Python's strengths is its diverse community."
+print(message)
+
+#2-3. Personal Message: Use a variable to represent a person’s name, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+personal_msg = "Hello Eric, would you like to learn some Python today?"
+print(personal_msg)
+
+#2-4. Name Cases: Use a variable to represent a person’s name, and then print that person’s name in lowercase, uppercase, and title case.
+name = "terry b cho"
+print(name.title())
+print(name.upper())
+print(name.lower())
+
+#2-5. Famous Quote: Find a quote from a famous person you admire. 
+#Print the quote and the name of its author. 
+#Your output should look something like the following, including the quotation marks:
+	#	Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+print("Albert Einstein once said, “A person who never made a mistake never tried anything new.")
+
+#2-6. Famous Quote 2: Repeat Exercise 2-5, but this time, represent the famous person’s name using a variable called famous_person. 
+#Then compose your message and represent it with a new variable called message. Print your message.
+famous_person = "Albert Einstein"
+
+
+person_name = "\tterry cho\n"
+print(person_name)
+print(person_name.strip())
